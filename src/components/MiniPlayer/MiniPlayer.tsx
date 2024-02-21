3e474335302e349b0f7be54aa323066a086bd5ba
Muhammad Mujtahid <mujtahid705@gmail.com>
2024-02-22 02:55:54 +0600

    music play screen implemented

--- src/components/MiniPlayer/MiniPlayer.tsx
+++ src/components/MiniPlayer/MiniPlayer.tsx
@@ -1,19 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { IconButton } from '@react-native-material/core'
-import { MediaGoPreviousIcon, MediaGoNextIcon, MediaPauseIcon, MediaPlayIcon, MediaReplayIcon } from '../../../assets/Icons'
-import Slider from '@react-native-community/slider'
-import React, { useState } from 'react'
-import { useTheme } from '../Theme'
-import CustomText from '../../atoms/Typography/CustomText'
-import { CurrentMusicSeeker } from '../CurrentMusicSeeker'
-import { CurrentMusicDuration } from '../CurrentMusicDuration'
-import { CurrentMusicActionButtons } from '../CurrentMusicActionButtons/CurrentMusicActionButtons'
+import { StyleSheet, Text, View } from "react-native";
+import { IconButton } from "@react-native-material/core";
+import {
+  MediaGoPreviousIcon,
+  MediaGoNextIcon,
+  MediaPauseIcon,
+  MediaPlayIcon,
+  MediaReplayIcon,
+} from "../../../assets/Icons";
+import Slider from "@react-native-community/slider";
+import React, { useState } from "react";
+import { useTheme } from "../Theme";
+import CustomText from "../../atoms/Typography/CustomText";
+import { CurrentMusicSeeker } from "../CurrentMusicSeeker";
+import { CurrentMusicDuration } from "../CurrentMusicDuration";
+import { CurrentMusicActionButtons } from "../CurrentMusicActionButtons/CurrentMusicActionButtons";
 
-export function MiniPlayer({ data, status, onPause, onPlay, onReplay } : PropTypes.MiniPlayer ) {
-  const theme = useTheme()
+export function MiniPlayer({
+  data,
+  status,
+  onPause,
+  onPlay,
+  onReplay,
+}: PropTypes.MiniPlayer) {
+  const theme = useTheme();
   const styles = themedStyles(theme);
   // const [paused, setPaused] = useState(true);
-  
+
   return (
     <View>
       <View style={styles.playerInfo}>
@@ -29,33 +41,36 @@ export function MiniPlayer({ data, status, onPause, onPlay, onReplay } : PropTyp
           {(status === "finished") && <IconButton onPress={() => onReplay()} icon={<MediaReplayIcon color={theme.colors?.primary} />}></IconButton>}
           <IconButton icon={<MediaGoNextIcon />} />
         </View> */}
-        <CurrentMusicActionButtons style={styles.playerNavButtons} />
+        <CurrentMusicActionButtons
+          style={styles.playerNavButtons}
+          varient="mini"
+        />
       </View>
       <View>
         <CurrentMusicSeeker />
       </View>
     </View>
-  )
+  );
 }
 
-const themedStyles = (theme : Config.Theme) => {
+const themedStyles = (theme: Config.Theme) => {
   return StyleSheet.create({
     playerInfo: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between'
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
     },
     title: {
       fontSize: 16,
-      fontWeight: '300',
-      color: '#ffffff'
+      fontWeight: "300",
+      color: "#ffffff",
     },
     timestamp: {
       color: theme.colors?.primaryLight,
-      fontSize: 12
+      fontSize: 12,
     },
     playerNavButtons: {
-      flexDirection: 'row'
-    }
-  })
-}
+      flexDirection: "row",
+    },
+  });
+};
